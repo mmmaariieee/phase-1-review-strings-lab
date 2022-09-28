bdb6c82535cc05b749b32dc2c539a80d8338ac37
Mariia Honcharenko <m.goncharenkoo@icloud.com>
2022-09-28 00:33:50 -0400

    commit tree c400b438ddbe12f240dd9c0597b55cf99f9f626f

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code in this file!
+// Write your code in this file! How are you Mariia?
+const currentUser = 'Grace Hopper';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
